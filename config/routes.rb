2e5c2251ae832127103090d226eb192703522d5f
Jasper Brumter <jasper.brumter@mail.mcgill.ca>
2018-11-06 17:49:13 -0500

    added task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/new", to: 'tasks#new', as: "new"
+  post "/tasks", to: 'tasks#create', as: "create"
+  get "/tasks", to: 'tasks#index'
+  get "/tasks/:id", to: 'tasks#show', as: "task"
+  get "/task/:id/edit", to: 'tasks#edit', as: "edit"
+
+  patch "/tasks/:id", to: 'tasks#update', as: "update"
+
+  delete "/tasks/:id/destroy", to: "tasks#destroy", as: "destroy"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+50.times do
+  Task.new({ title: Faker::Educator.course, details: Faker::University.name }).save
+end
